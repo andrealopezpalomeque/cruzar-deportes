74e66c255744b3b654c1f3ea8813ed043b315560
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2025-09-11 01:09:05 -0300

    use short logo in category cards

--- home/pages/categories/index.vue
+++ home/pages/categories/index.vue
@@ -24,7 +24,7 @@
             <div class="absolute inset-0 bg-black/20 group-hover:bg-black/10 transition-colors"></div>
             <div class="absolute inset-0 flex items-center justify-center">
               <img 
-                src="/images/cruzar-logo-no-bg.png" 
+                src="/images/cruzar-logo-short-1.png" 
                 alt="Cruzar Deportes" 
                 class="h-24 w-auto brightness-0 invert opacity-90"
               />
