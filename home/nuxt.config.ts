d713635c764b1f1b0b5c18dcd72744fff7c59ee6
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2025-09-19 08:51:44 -0300

    fixes '+' button behavior in ProductCard component

--- home/nuxt.config.ts
+++ home/nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'firebase'
+  },
+  ssr: false,
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
