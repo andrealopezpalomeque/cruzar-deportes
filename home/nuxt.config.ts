10c525b6156ef5631d8e6f8313dc3ded1e828a38
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2025-09-24 17:50:21 -0300

    Change Nitro preset from firebase to static
    Updated the Nitro preset in nuxt.config.ts to 'static' instead of 'firebase' to adjust deployment configuration.

--- home/nuxt.config.ts
+++ home/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
-    preset: 'firebase'
+    preset: 'static'
   },
   ssr: false,
   modules: [
